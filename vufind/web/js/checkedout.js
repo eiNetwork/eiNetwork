bc2ac116c08c7a6e7d2cd5e57f857cd6293bb339
June Rayner <vufind@vufindplus.einetwork.net>
2013-04-08 11:04:56 -0400

    Fix popup message if no items selected for renewal showing up on holds page

---
+++ vufind/web/js/checkedout.js
@@ -0,0 +1,17 @@
+function getSelectedTitles(){
+	var selectedTitles = $("input.titleSelect:checked ").map(function() {
+		return $(this).attr('name') + "=" + $(this).val();
+	}).get().join("&");
+	if (selectedTitles.length == 0){
+		var ret = alert('You have not selected any items, please select items to renew');
+	}
+	return selectedTitles;
+}
+
+function renewSelectedTitles(){
+	var selectedTitles = getSelectedTitles();
+	if (selectedTitles.length > 0){ 
+		$('#renewForm').submit()
+	}
+	return false;
+}
