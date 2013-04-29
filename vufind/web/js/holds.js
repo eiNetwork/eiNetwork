331e23db48fddf83da76dc08e97d962ad37dc462
June Rayner <vufind@vufindplus.einetwork.net>
2013-04-29 15:09:16 -0400

    markd - added freezeall

--- vufind/web/js/holds.js
+++ vufind/web/js/holds.js
@@ -2,7 +2,41 @@ $(document).ready(function() {
 	$('#update-selected-btn').click(function(){
 		updateSelected();
 	});
+	
+	$('#freeze-all-btn').click(function(){
+		freezeAll();
+	});
 });
+
+function freezeAll(){
+	
+	var inputs = $('.freeze_checkboxes');
+
+    var values = {};
+    
+    inputs.each(function() {
+    	
+    	if ($(this).is(':checked') ){
+    		$(this).val('on')
+    	} else {
+    		$(this).val('on')
+    	}
+    	
+    	values[this.name] = $(this).val();
+    });
+    
+    showProcessingIndicator('Attempting to freeze all your items. This may take a minute.');
+    
+    $.ajax({
+		type: 'POST',
+		url: '/MyResearch/Holds/',
+		data: values,
+		success: function(data) {
+				document.location.href='/MyResearch/Holds';
+		}
+	});
+	
+}
 
 function updateSelected(){
 	
